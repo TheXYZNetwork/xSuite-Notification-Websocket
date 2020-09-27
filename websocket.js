24e20c473a781183e0e828d5587d86cb6086f134
Tomsci <tomsciteam@gmail.com>
2020-09-27 20:41:08 +0100

    Added notification websocket

---
+++ websocket.js
@@ -0,0 +1,63 @@
+const https = require("https");
+const websocket = require("ws");
+const mysql = require("mysql");
+const cache = new Map();
+
+let SECRET_KEY = ""
+
+const httpsServer = https.createServer({});
+
+const ws = new websocket.Server({
+    server: httpsServer,
+    port: 8880
+});
+
+let database = mysql.createPool({
+    host: "localhost",
+    user: "site_xsuite",
+    password: "***REMOVED***",
+    database: "xsuite_master"
+});
+
+console.log("Starting Websocket!");
+
+function getCookie(cookie, cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while(c.charAt(0) == " ") {
+            c = c.substring(1);
+        };
+        if(c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        };
+    };
+    return "";
+};
+
+ws.on("connection", function(ws, request) {
+    if(request.headers.cookie && request.headers.cookie.includes(SECRET_KEY)) {
+        ws.on("message", message => {
+            message = message.split(",");
+            let userSocket = cache[message[0]];
+            if(userSocket && userSocket.readyState === websocket.OPEN) {
+                userSocket.send(message[1]);
+            };
+        });
+    } else {
+        let rawcookie = getCookie(request.headers.cookie, "xyz_session");
+        if(!request.headers.cookie) {
+            ws.close(); 
+            console.log(`No cookies from ${ws._socket.remoteAddress}`); 
+            return;
+        };
+
+        database.query(`SELECT * FROM sessions WHERE token="${rawcookie}"`, function (error, result, fields) {
+            if(error) return console.log(error);
+            if(!result) return console.log(`No session token found for ${ws._socket.remoteAddress} trying to use ${rawcookie}!`);
+            cache[result[0].userid] = ws;
+        });
+    };
+});
